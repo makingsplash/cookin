9b043346c0ad70bd6f66376fd262a5161f6c2f93
Ilya Bogdan <ilya_bogdan@belkatechnologies.com>
2023-02-21 13:54:47 +0100

    spawn guest only if have orders to assign

--- Assets/Scripts/Core/Game/Play/ECS/Systems/ExecuteSystems/SpawnGuestsSystem.cs
+++ Assets/Scripts/Core/Game/Play/ECS/Systems/ExecuteSystems/SpawnGuestsSystem.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Core.Game.Play.Configs;
 using Core.UI.Elements;
 using Cysharp.Threading.Tasks;
@@ -13,16 +14,18 @@ namespace Core.Game.Play.ECS.Systems.ExecuteSystems
         private readonly GameContext _context;
         private readonly LevelConfig _levelConfig;
         private readonly Transform _guestsRoot;
+        private readonly LevelDishes _levelDishes;
 
         private readonly IGroup<GameEntity> _group;
         private int _spawnedGuestsAmount;
         private float _timeToSpawn;
 
-        public SpawnGuestsSystem(GameContext context, LevelConfig levelConfig, UIRoot uiRoot)
+        public SpawnGuestsSystem(GameContext context, LevelConfig levelConfig, UIRoot uiRoot, LevelDishes levelDishes)
         {
             _context = context;
             _levelConfig = levelConfig;
             _guestsRoot = uiRoot.GuestsRoot;
+            _levelDishes = levelDishes;
             _group = _context.GetGroup(GameMatcher.PlayECSUnservedGuest);
             _group.OnEntityAdded += IncreaseSpawnedGuestsAmount;
             _group.OnEntityRemoved += DecreaseSpawnedGuestsAmount;
@@ -40,17 +43,20 @@ namespace Core.Game.Play.ECS.Systems.ExecuteSystems
 
         public void Execute()
         {
-            if (_timeToSpawn > 0)
+            if (_levelDishes.DishesToAssign.Any())
             {
-                _timeToSpawn -= Time.deltaTime;
-            }
-            else
-            {
-                if (_spawnedGuestsAmount < _levelConfig.GuestsSpawnAmount)
+                if (_timeToSpawn > 0)
                 {
-                    _timeToSpawn = _levelConfig.GuestsSpawnRate;
+                    _timeToSpawn -= Time.deltaTime;
+                }
+                else
+                {
+                    if (_spawnedGuestsAmount < _levelConfig.GuestsSpawnAmount)
+                    {
+                        _timeToSpawn = _levelConfig.GuestsSpawnRate;
 
-                    SpawnGuest().Forget();
+                        SpawnGuest().Forget();
+                    }
                 }
             }
         }
